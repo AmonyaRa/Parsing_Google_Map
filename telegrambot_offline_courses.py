a1e1855df5c55cec5b37fe3a781a2721ca6404b9
AmonyaRa <112092451+AmonyaRa@users.noreply.github.com>
2023-01-09 08:23:00 +0600

    Update telegrambot_offline_courses.py

--- telegrambot_offline_courses.py
+++ telegrambot_offline_courses.py
@@ -5,7 +5,7 @@ from test import *
 
 URL = 'https://www.google.com/maps/search/%D1%8F%D0%B7%D1%8B%D0%BA%D0%BE%D0%B2%D1%8B%D0%B5+%D0%BA%D1%83%D1%80%D1%81%D1%8B+%D0%B2+%D0%91%D0%B8%D1%88%D0%BA%D0%B5%D0%BA%D0%B5/@42.8760111,74.5856421,14z/data=!3m1!4b1'
 
-bot = telebot.TeleBot('5876023372:AAHBK5lmPJ-wPxsV3dgE0b6Kqcpr5uxa6UM')
+bot = telebot.TeleBot(token_bot)
 choice = types.InlineKeyboardMarkup()
 button = types.InlineKeyboardButton('Courses', callback_data='courses')
 choice.add(button)
